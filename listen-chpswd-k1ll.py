f993a812456d95bf30ab70731907e1ea70d04e6a
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2025-03-06 13:33:47 -0500

    made some fixes, added chpasswd as well

---
+++ listen-chpswd-k1ll.py
@@ -0,0 +1,19 @@
+import socket
+
+def start_listener():
+    host = '0.0.0.0'
+    port = 37421
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+    print("Listener started...")
+
+    while True:
+        conn, addr = s.accept()
+        print(f"Connection from {addr}")
+        data = conn.recv(1024)
+        print(data.decode('utf-8'))
+        conn.close()
+
+if __name__ == "__main__":
+    start_listener()
